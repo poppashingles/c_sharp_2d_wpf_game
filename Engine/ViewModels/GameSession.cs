a485c1d282fd01c509cd661f96aeb3f733fb9e23
poppashingles <robdwilliams85@gmail.com>
2019-09-16 15:11:31 +0100

    Added the player class, game session class and linked to xaml files. Displaying player information in main screen

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Jimothy";
+            CurrentPlayer.CharacterClass = "Fighter";
+            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
+            CurrentPlayer.Gold = 1000000;
+        }
+    }
+}
