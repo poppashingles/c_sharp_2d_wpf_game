ee88d6258dcd7ae33c8594668f1c624e91924f02
poppashingles <robdwilliams85@gmail.com>
2019-09-19 16:18:28 +0100

    Players can now attack encountered monsters and receive loot, gold and xp

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -55,24 +55,7 @@ namespace Engine.ViewModels
             }
         }
 
-        public bool HasMonster => CurrentMonster != null;
-
-        public GameSession()
-        {
-            CurrentPlayer = new Player
-            {
-                Name = "Jimothy",
-                CharacterClass = "Fighter",
-                HitPoints = 10,
-                ExperiencePoints = 0,
-                Level = 1,
-                Gold = 1000000
-            };
-
-            CurrentWorld = WorldFactory.CreateWorld();
-
-            CurrentLocation = CurrentWorld.LocationAt(0, 0);
-        }
+        public Weapon CurrentWeapon { get; set; }
 
         public bool HasLocationToNorth
         {
@@ -104,6 +87,30 @@ namespace Engine.ViewModels
             {
                 return CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate) != null;
             }
+        }
+
+        public bool HasMonster => CurrentMonster != null;
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player
+            {
+                Name = "Jimothy",
+                CharacterClass = "Fighter",
+                HitPoints = 10,
+                ExperiencePoints = 0,
+                Level = 1,
+                Gold = 1000000
+            };
+
+            if(!CurrentPlayer.Weapons.Any())
+            {
+                CurrentPlayer.AddItemToInventory(ItemFactory.CreateGameItem(1001));
+            }
+
+            CurrentWorld = WorldFactory.CreateWorld();
+
+            CurrentLocation = CurrentWorld.LocationAt(0, 0);
         }
 
         public void MoveNorth()
@@ -152,6 +159,71 @@ namespace Engine.ViewModels
         private void GetMonsterAtLocation()
         {
             CurrentMonster = CurrentLocation.GetMonster();
+        }
+
+        public void AttackCurrentMonster()
+        {
+            if(CurrentWeapon == null)
+            {
+                RaiseMessage("You must select a weapon to attack");
+                return;
+            }
+
+            int damageToMonster = RandomNumberGenerator.NumberBetween(CurrentWeapon.MinimumDamage, CurrentWeapon.MaximumDamage);
+
+            if (damageToMonster == 0)
+            {
+                RaiseMessage($"You missed the {CurrentMonster.Name}");
+            }
+            else
+            {
+                CurrentMonster.HitPoints -= damageToMonster;
+                RaiseMessage($"You hit the {CurrentMonster.Name} for {damageToMonster} points");
+            }
+
+            if(CurrentMonster.HitPoints <= 0)
+            {
+                RaiseMessage("");
+                RaiseMessage($"You defeated the {CurrentMonster.Name}");
+
+                CurrentPlayer.ExperiencePoints += CurrentMonster.RewardExperiencePoints;
+                RaiseMessage($"You receive {CurrentMonster.RewardExperiencePoints} xp");
+
+                CurrentPlayer.Gold += CurrentMonster.RewardGold;
+                RaiseMessage($"You receive {CurrentMonster.RewardGold} gold");
+
+                foreach (ItemQuantity itemQuantity in CurrentMonster.Inventory)
+                {
+                    GameItem item = ItemFactory.CreateGameItem(itemQuantity.ItemID);
+                    CurrentPlayer.AddItemToInventory(item);
+                    RaiseMessage($"You receive {itemQuantity.Quantity} {item.Name}");
+                }
+
+                GetMonsterAtLocation();
+            }
+            else
+            {
+                int damageToPlayer = RandomNumberGenerator.NumberBetween(CurrentMonster.MinimumDamage, CurrentMonster.MaximumDamage);
+
+                if(damageToPlayer == 0)
+                {
+                    RaiseMessage("The monster attacks, but missed you");
+                }
+                else
+                {
+                    CurrentPlayer.HitPoints -= damageToPlayer;
+                    RaiseMessage($"The {CurrentMonster.Name} hit you for {damageToPlayer} points.");
+                }
+
+                if(CurrentPlayer.HitPoints <= 0)
+                {
+                    RaiseMessage("");
+                    RaiseMessage($"The {CurrentMonster.Name} killed you");
+
+                    CurrentLocation = CurrentWorld.LocationAt(0, -1);
+                    CurrentPlayer.HitPoints = CurrentPlayer.Level * 10;
+                }
+            }
         }
 
         private void RaiseMessage(string message)
